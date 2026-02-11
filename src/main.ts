c7456327cb82ae711fc53d0fd15d1e3c473b9852
Toshiyuki Masui <masui@pitecan.com>
2026-02-11 11:04:09 +0900

    ...

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { initMap } from "./map";
 async function main() {
   console.log("Mapfeel starting...");
 
-  //const map = initMap();
-  initMap();
+  const map = initMap();
 
   // ここに将来:
-  // const pages = await fetchScrapbox(...)
+  const aboutRes = await fetch(`https://scrapbox.io/api/projects/masuimap/`);
+  const aboutJson = await aboutRes.json();
+  console.log(aboutJson)
+
   // const images = extractGyazo(...)
   // addImagesToMap(map, images)
 }
